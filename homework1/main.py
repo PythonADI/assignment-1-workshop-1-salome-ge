5788b133131aab474b25adb14ff92a79a5676208
salome-ge <salome82k@gmail.com>
2025-01-21 00:06:08 +0400

    Update main.py

--- homework1/main.py
+++ homework1/main.py
@@ -1,5 +1,27 @@
 def main() -> None:
     # Write your solution here
+    first_name = "Salome"
+last_name = "Khukhunaishvili"
+full_name = first_name + ' ' + last_name
+print(full_name) 
+# variable full_name is string
+
+age = 20
+print('You are',age,'years old')
+# variable age is intiger
+
+temperature = 20.3
+print('The temperature is', temperature,'degrees')
+# variable temperature is float
+
+is_hot = temperature > 30
+print(is_hot)
+# variable is_hot is boolean
+
+# int - Integer, რიცხვითი მნიშვნელობა(14)
+# float - Floating-point number, წილადი რიცხვები(17,8)	
+# str - String, ტექსტური ცვლადი("Salome")
+# bool - Boolean, ლოგიკური ცვლადი(true, false)
     pass
 
 
